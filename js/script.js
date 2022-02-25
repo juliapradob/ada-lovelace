cefede17b0bbf313764eb96b59b31cefa439012b
juliapradob <99263001+juliapradob@users.noreply.github.com>
2022-02-25 20:35:55 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.getElementById("botaoEnviar").addEventListener("click", validaFormulario)
+
+function validaFormulario (){
+  if (document.getElementById("nome").value != "" && 
+      document.getElementById("email").value != "" && 
+      document.getElementById("telefone").value != ""){
+        alert("Prontinho! Você receberá as novidades por e-mail.")
+  }
+  else{
+        alert("Por favor, preencha os campos nome, telefone e-mail!")
+  } 
+}
